a2c4335e624a037bead65d7477048ddd7ded9f39
SLebailly <s9sileba@stud.uni-saarland.de>
2021-03-18 19:38:13 +0100

    cudatest

--- cudatest.py
+++ cudatest.py
@@ -3,9 +3,9 @@ import torch
 print("testing availability of CUDA...")
 print(torch.cuda.device_count())
 print(torch.cuda.current_device())
-#sys.stdout.flush()
+sys.stdout.flush()
 
 device = torch.device('cuda')
 a = torch.randn(10, 10, device=device)
 
-#os.system('nvidia-smi')
+os.system('nvidia-smi')
