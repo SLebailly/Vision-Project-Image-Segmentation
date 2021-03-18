5a286e328a860789e8403b7256a6bdaea327ce5d
SLebailly <s9sileba@stud.uni-saarland.de>
2021-03-18 19:46:48 +0100

    updated CUDA_VISIBLE_DEVICES

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export PYTHONPATH="$PYTHONPATH:/Vision-Project-Image-Segmentation"
 # Passed as arguments
 export CUDA_VISIBLE_DEVICES=$3
 
-jupyter notebook --ip 0.0.0.0 --port 8890 --allow-root
+CUDA_VISIBLE_DEVICES=3 jupyter notebook --ip 0.0.0.0 --port 8890 --allow-root
