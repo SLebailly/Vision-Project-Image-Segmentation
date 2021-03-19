6e9d7ba09f0966600b8bf492d076b84d33552fac
SLebailly <s9sileba@stud.uni-saarland.de>
2021-03-19 03:45:06 +0100

    adapted run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export PYTHONPATH="$PYTHONPATH:/Vision-Project-Image-Segmentation"
 
-CUDA_VISIBLE_DEVICES=7 jupyter notebook --ip 0.0.0.0 --port 8890 --allow-root
+CUDA_VISIBLE_DEVICES=6 jupyter notebook --ip 0.0.0.0 --port 8890 --allow-root
